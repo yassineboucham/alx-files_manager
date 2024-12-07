3c56e32b574713a9513ec48eb5783816714edf4d
yassineboucham <yassineboucham2002@gmail.com>
2024-12-07 12:49:08 +0100

    serve

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
